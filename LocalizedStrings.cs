664f36977c4fc80eed8106c5f0e6ed163f8d821c
Alexander Bobryshev <alexanderbob@live.com>
2014-04-04 16:32:51 +0400

    first release

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ssau_handbook_cs.Resources;
+
+namespace ssau_handbook_cs
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
